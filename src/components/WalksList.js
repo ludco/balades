72fb71dc690cdc4e70cf687e966d389fd25d452c
Ludivine COUJARD <ludivine.coujard@apside-groupe.com>
2021-07-07 14:47:31 +0200

    scroll walk list

--- src/components/WalksList.js
+++ src/components/WalksList.js
@@ -1,12 +1,15 @@
 import React from 'react';
+
 import { Walk } from './Walk';
 
 export const WalksList = ({ walks, history }) => {
   return (
-    <div>
-      {walks.map((walk) => {
-        return <Walk key={walk.name} walk={walk} history={history} />;
-      })}
+    <div id="listContainer">
+      <div id="list">
+        {walks.map((walk) => {
+          return <Walk key={walk.name} walk={walk} history={history} />;
+        })}
+      </div>
     </div>
   );
 };
